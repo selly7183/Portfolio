f9aca1b0071dceae5d362b34bfcd94d95cf564b7
selly7183 <selly7183@naver.com>
2022-12-01 00:13:54 +0900

    css : about me

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const n1 = document.querySelectorAll(".n01");
+const n2 = document.querySelectorAll(".n02");
+const n3 = document.querySelectorAll(".n03");
+
+setTimeout(function () {
+	n2.addClass("on");
+}, 380);
+setTimeout(function () {
+	n3.classList.addEventListner.add("on");
+}, 430);
