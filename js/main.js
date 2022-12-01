67991fdf2d82137ea7fa31680dfeb0ca27b51fd4
selly7183 <selly7183@naver.com>
2022-12-01 23:17:41 +0900

    js navbar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-const n1 = document.querySelectorAll(".n01");
-const n2 = document.querySelectorAll(".n02");
-const n3 = document.querySelectorAll(".n03");
+"use strict";
 
-setTimeout(function () {
-	n2.addClass("on");
-}, 380);
-setTimeout(function () {
-	n3.classList.addEventListner.add("on");
-}, 430);
+//header navbar transparent when it is on the top
+const header = document.querySelector("#header");
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+	// console.log(window.scrollY);
+	// console.log(`headerHeight: ${headerHeight}`);
+
+	if (window.scrollY > headerHeight) {
+		header.classList.add("header--dark");
+	} else {
+		header.classList.remove("header--dark");
+	}
+});
+
+// handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+	const target = e.target;
+	const link = target.dataset.link;
+	if (link == null) {
+		return;
+	}
+	const scrollTo = document.querySelector(link);
+	scrollTo.scrollIntoView({
+		behavior: "smooth",
+	});
+});
