b54e1b31ac207f0e946b22a51fad345bf619fd54
selly7183 <selly7183@naver.com>
2022-12-02 08:46:39 +0900

    js scrollTop

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@
 const header = document.querySelector("#header");
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
-	// console.log(window.scrollY);
-	// console.log(`headerHeight: ${headerHeight}`);
-
 	if (window.scrollY > headerHeight) {
 		header.classList.add("header--dark");
 	} else {
@@ -31,15 +28,30 @@ homeContact.addEventListener("click", () => {
 	scrollTo("#contact");
 });
 
+// scrolling home background opacity down
+const home = document.querySelector("#home");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+	home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// show scrollTop button when scrolling down
+const scrollTop = document.querySelector(".scroll__top");
+document.addEventListener("scroll", () => {
+	if (window.scrollY > homeHeight / 2) {
+		scrollTop.classList.add("on");
+	} else {
+		scrollTop.classList.remove("on");
+	}
+});
+// handle click on'the scrollTop' button on home
+scrollTop.addEventListener("click", () => {
+	scrollTo("#home");
+});
+
 function scrollTo(selector) {
 	const scrollTo = document.querySelector(selector);
 	scrollTo.scrollIntoView({
 		behavior: "smooth",
 	});
 }
-
-// scrollTop
-const scrollTop = document.querySelector(".scroll__top");
-scrollTop.addEventListener("click", () => {
-	scrollTo("#home");
-});
