a8adf239568e12b28a071aa961bebbc93d8a00a8
selly7183 <selly7183@naver.com>
2022-12-04 16:21:21 +0900

    js remove active nav

--- js/main.js
+++ js/main.js
@@ -58,6 +58,14 @@ workCategory.addEventListener("click", (e) => {
 	if (filter == null) {
 		return;
 	}
+
+	//remove selection from the previous item and select the new one
+	const active = document.querySelector(".category__btn.selected");
+	active.classList.remove("selected");
+	const target =
+		e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+	target.classList.add("selected");
+
 	projectWork.classList.add("anim-out");
 	setTimeout(() => {
 		projects.forEach((project) => {
